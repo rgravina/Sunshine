ffcdd42547e5c427b159330a1410b3718b885eb3
Robert Gravina <rgravina@pivotal.io>
2018-05-23 17:49:26 +0900

    Add test for MainActivity

---
+++ app/src/androidTest/java/com/example/android/sunshine/MainActivityTest.kt
@@ -0,0 +1,32 @@
+package com.example.android.sunshine
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.hamcrest.CoreMatchers.containsString
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    @Rule
+    @JvmField
+    val rule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun app_containsWeatherList() {
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.android.sunshine", appContext.packageName)
+        onView(withId(R.id.tv_weather_data))
+                .check(matches(withText(containsString("Today, May 17 - Clear - 17°C / 15°C"))))
+    }
+}
